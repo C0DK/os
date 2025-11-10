4f6461ea27b48f2480039f33dc9117c70e08b01b
Casper Weiss Bang <c@cwb.dk>
2025-11-10 22:12:50 +0100

    chore: remove nur as not used

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nur.url = "github:nix-community/NUR";
-    nur.inputs.nixpkgs.follows = "nixpkgs";
-
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
